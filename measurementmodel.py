253e9a4d952b1e78c520890660cdb622f3eeadea
Avi Singh <avisingh0718@gmail.com>
2026-03-03 13:57:37 -0800

    added gyro measurements

--- measurementmodel.py
+++ measurementmodel.py
@@ -1,8 +1,10 @@
 import numpy as np
 
 class MeasurementModel:
-    def __init__(self, range_std=0.1, angle_std=0.1):
+    def __init__(self, range_std=0.1, angle_std=0.1, gyro_std=0.01):
         self.R = np.diag([range_std**2, np.deg2rad(angle_std)**2, np.deg2rad(angle_std)**2])
+        self.R_gyro = np.diag([gyro_std**2, gyro_std**2, gyro_std**2])
+        self.gyro_bias = np.array([0.001, -0.002, 0.001]) # small static bias
 
     def get_relative_measurement(self, observer_state, target_pos, add_noise=False):
         px, py, pz = observer_state[0:3]
@@ -20,5 +22,15 @@ class MeasurementModel:
             z += np.random.multivariate_normal(np.zeros(3), self.R)
             
         return z
+
+    def get_gyroscope_measurement(self, state, add_noise=False):
         
+        p, q, r = state[9:12]
+        gyro_true = np.array([p, q, r])
         
+        if add_noise:
+            gyro_meas = gyro_true + self.gyro_bias + np.random.multivariate_normal(np.zeros(3), self.R_gyro)
+        else:
+            gyro_meas = gyro_true + self.gyro_bias 
+            
+        return gyro_meas
--- sim.py
+++ sim.py
@@ -48,6 +48,11 @@ meas_links_leader = []
 meas_links_f1 = []
 meas_links_f2 = []
 
+# Initting gyroscope measurement history
+gyro_history_leader = []
+gyro_history_f1 = []
+gyro_history_f2 = []
+
 # Simulation loop
 for i in range(num_steps):
     leader.step_dynamics()
@@ -59,16 +64,58 @@ for i in range(num_steps):
     z_leader = meas_model.get_relative_measurement(leader.state, landmark_pos, add_noise=True)
     z_follower_1 = meas_model.get_relative_measurement(follower_1.state, leader.state[:3], add_noise=True)
     z_follower_2 = meas_model.get_relative_measurement(follower_2.state, leader.state[:3], add_noise=True)
+    
+    gyro_leader = meas_model.get_gyroscope_measurement(leader.state, add_noise=True)
+    gyro_follower_1 = meas_model.get_gyroscope_measurement(follower_1.state, add_noise=True)
+    gyro_follower_2 = meas_model.get_gyroscope_measurement(follower_2.state, add_noise=True)
+    
+    gyro_history_leader.append(gyro_leader)
+    gyro_history_f1.append(gyro_follower_1)
+    gyro_history_f2.append(gyro_follower_2)
 
     if i % 100 == 0:
         meas_links_leader.append((leader.state[:3].copy(), landmark_pos))
         meas_links_f1.append((follower_1.state[:3].copy(), leader.state[:3].copy()))
         meas_links_f2.append((follower_2.state[:3].copy(), leader.state[:3].copy()))
 
+gyro_history_leader = np.array(gyro_history_leader)
+gyro_history_f1 = np.array(gyro_history_f1)
+gyro_history_f2 = np.array(gyro_history_f2)
+
 ####################################################################
 ##############              PLOTTING BELOW           ###############
 ####################################################################
 # All plotting tools should just take the drone objects as inputs!
+
+# Plot Gyroscope Measurements
+fig_gyro, axs_gyro = plt.subplots(3, 1, figsize=(10, 8), sharex=True)
+fig_gyro.suptitle('Leader Drone Gyroscope Measurements vs True Body Rates')
+times = leader.get_times()[:-1]  # Exclude last time step to match measurement length
+
+true_states = leader.get_state_time_history()[:-1]
+true_p, true_q, true_r = true_states[:, 9], true_states[:, 10], true_states[:, 11]
+
+# P (leader)
+axs_gyro[0].plot(times, true_p, label='True $p$', color='black', linewidth=2)
+axs_gyro[0].plot(times, gyro_history_leader[:, 0], label='Measured $p$', color='red', alpha=0.5)
+axs_gyro[0].set_ylabel('Roll Rate (rad/s)')
+axs_gyro[0].legend()
+axs_gyro[0].grid(True)
+
+# Q (leader)
+axs_gyro[1].plot(times, true_q, label='True $q$', color='black', linewidth=2)
+axs_gyro[1].plot(times, gyro_history_leader[:, 1], label='Measured $q$', color='green', alpha=0.5)
+axs_gyro[1].set_ylabel('Pitch Rate (rad/s)')
+axs_gyro[1].legend()
+axs_gyro[1].grid(True)
+
+# R (leader)
+axs_gyro[2].plot(times, true_r, label='True $r$', color='black', linewidth=2)
+axs_gyro[2].plot(times, gyro_history_leader[:, 2], label='Measured $r$', color='blue', alpha=0.5)
+axs_gyro[2].set_ylabel('Yaw Rate (rad/s)')
+axs_gyro[2].set_xlabel('Time (s)')
+axs_gyro[2].legend()
+axs_gyro[2].grid(True)
 
 # All States and Controls Plots!
 fig1 = utils.plot_states_and_controls(leader, title='Leader Drone')
