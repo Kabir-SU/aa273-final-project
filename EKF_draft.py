a337d0b0fbdbfbf74e4a466529eda914263dacda
julia-feldhaus <jcfeld@stanford.edu>
2026-03-03 14:03:52 -0800

    Start of centralized approach

--- EKF_draft.py
+++ EKF_draft.py
@@ -7,6 +7,7 @@ g = 9.81
 jphi, jtheta, jpsi = 10, 10, 5
 
 def truedynamics(state, u): 
+    #Compute the simulated next state using euler approximation on a single drone dynamics model
     F, tauphi, tautheta, taupsi = u
     px, py, pz, vx, vy, vz, phi, theta, psi, dphi, dtheta, dpsi = state
     step = np.zeros_like(state)
@@ -20,6 +21,7 @@ def truedynamics(state, u):
     return step
 
 def dynamics_model_joint(state, u):
+    #Compute the simulated next state for the full 3 drone state model
     nextstate = np.zeros_like(state)
     for i in range(0, 3):
         stepstate = truedynamics(state[i*12 : (i+1)*12], u[i*4 : (i+1)*4])
@@ -27,6 +29,7 @@ def dynamics_model_joint(state, u):
     return nextstate
 
 def getG_single(state, u):
+    #Compute the state dynamics jacobian for a single drone
     F, tauphi, tautheta, taupsi = u
     px, py, pz, vx, vy, vz, phi, theta, psi, dphi, dtheta, dpsi = state
 
@@ -35,59 +38,36 @@ def getG_single(state, u):
     cpsi = np.cos(psi);   spsi = np.sin(psi)
 
     G = np.eye(12)
-
-    G[0,0] = 1.0; G[0,3] = dt
-    G[1,1] = 1.0; G[1,4] = dt
-    G[2,2] = 1.0; G[2,5] = dt
-
-    # -----------------
-    # Velocity updates
-    # -----------------
-
-    # Precompute projection terms
-    A = (-cphi*sth*cpsi - sphi*spsi)
-    B = (-cphi*sth*spsi + sphi*cpsi)
-
-    # Derivatives of A
-    dA_dphi   =  sphi*sth*cpsi - cphi*spsi
-    dA_dtheta = -cphi*cth*cpsi
-    dA_dpsi   =  cphi*sth*spsi - sphi*cpsi
-
-    # Derivatives of B
-    dB_dphi   =  sphi*sth*spsi + cphi*cpsi
+    G[0,3] = dt
+    G[1,4] = dt
+    G[2,5] = dt
+    
+    dA_dphi = (sphi*sth*cpsi - cphi*spsi)
+    dA_dtheta = (-cphi*cth*cpsi)
+    dA_dpsi   = (cphi*sth*spsi - sphi*cpsi)
+    dB_dphi = (sphi*sth*spsi + cphi*cpsi)
     dB_dtheta = -cphi*cth*spsi
     dB_dpsi   = -cphi*sth*cpsi - sphi*spsi
 
-    # vx
-    G[3,3] = 1.0
     G[3,6] = dt*(F/m)*dA_dphi
     G[3,7] = dt*(F/m)*dA_dtheta
     G[3,8] = dt*(F/m)*dA_dpsi
 
-    # vy
-    G[4,4] = 1.0
     G[4,6] = dt*(F/m)*dB_dphi
     G[4,7] = dt*(F/m)*dB_dtheta
     G[4,8] = dt*(F/m)*dB_dpsi
 
-    # vz
-    G[5,5] = 1.0
-    G[5,6] = dt*(F/m)*( sphi*cth )
-    G[5,7] = dt*(F/m)*( cphi*sth )
-    # no psi dependence
+    G[5,6] = dt*(F/m)*(sphi*cth)
+    G[5,7] = dt*(F/m)*(cphi*sth)
 
-    # -----------------
-    # Angle updates
-    # -----------------
-    G[6,6] = 1.0; G[6,9]  = dt
-    G[7,7] = 1.0; G[7,10] = dt
-    G[8,8] = 1.0; G[8,11] = dt
-
-    # -----------------
-    # Angular rate updates
-    # -----------------
-    G[9,9]   = 1.0
-    G[10,10] = 1.0
-    G[11,11] = 1.0
-
+    G[6,9]  = dt
+    G[7,10] = dt
+    G[8,11] = dt
     return G
+def get_G_full(state, u):
+    Z = np.zeros((36, 36))
+    A = getG_single(state[0:12], u[0:4])
+    B = getG_single(state[12:24], u[4:8])
+    C = getG_single(state[24:36], u[8:12]) 
+    
+    return np.block([[A, Z, Z], [Z, B, Z], [Z, Z, C]])
